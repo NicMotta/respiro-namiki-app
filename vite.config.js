51f3f27889cf665f2ef2205505ddc21ad62cff7a
Nic Motta <nic.motta@accenture.com>
2023-07-09 11:23:52 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  site: 'https://nicmotta.github.io',
-  base: './respiro-namiki-app',
+  base: './',
   plugins: [
     svelte(),
     VitePWA({
